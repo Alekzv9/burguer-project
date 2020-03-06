3b522039f46ffd565f3f5ea7a6a1b61aedfdf72f
Erick Valdivia <Erick_Valdivia@epam.com>
2020-03-06 13:22:31 -0600

    Reusing the Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
 import classes from './SideDrawer.module.css';
 
 const sideDrawer = props => {
+  let attachedClasses = [classes.sideDrawer, classes.close];
+  if (props.open) {
+    attachedClasses = [classes.sideDrawer, classes.open];
+  }
   return (
-    <div className={classes.sideDrawer}>
-      <div className={classes.logo}>
-        <Logo />
+    <Auxiliary>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Auxiliary>
   );
 };
 
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = props => (
-  //   <React.Fragment> // Alternative to HOC Auxiliary component.
-  <Auxiliary>
-    <SideDrawer />
-    <Toolbar />
-    <main className={classes.content}>{props.children}</main>
-  </Auxiliary>
-);
-export default layout;
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      //   <React.Fragment> // Alternative to HOC Auxiliary component.
+      <Auxiliary>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <Toolbar />
+        <main className={classes.content}>{this.props.children}</main>
+      </Auxiliary>
+    );
+  }
+}
+export default Layout;
